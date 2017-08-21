b03aacbf5fb179345ad332a129a20cbd8e052640
Jake Pearson <notjakepearson@gmail.com>
2017-08-20 19:09:57 -0600

    Matrix Element Sum

--- typescript/codeFights.ts
+++ typescript/codeFights.ts
@@ -1,131 +1,6 @@
-import { assert } from 'chai';
-
-function makeArrayConsecutive2(statues: number[]): number {
-  let keys: { [id: number]: boolean } = {};
-  let max = statues[0];
-  let min = statues[0];
-  statues.forEach(size => {
-    if (size > max) max = size;
-    if (size < min) min = size;
-    keys[size] = true;
-  });
-  let result = 0;
-  for (let i = min; i <= max; i++) {
-    if (!keys[i]) {
-      result++;
-    }
-  }
-  return result;
-}
-
-// function almostIncreasingSequence(sequence: number[]): boolean {
-//   console.log("Starting")
-//   let errors = 0;
-//   let current = sequence[0];
-//   for (let i = 0; i < sequence.length - 1; i++) {
-//     console.log(sequence[i] + " " + sequence[i + 1] + " " + sequence[i + 2] + ":" + current);
-//     if (sequence[i] < sequence[i + 1]) {
-//       console.log("Good");
-//       current = sequence[i+1];
-//     } else if (i === sequence.length - 2 && sequence[i] < sequence[i + 1] - 1) {
-//       console.log("Last");
-//       errors++;
-//     } else if (current < (sequence[i + 2] - 1)) {
-//       console.log("C");
-//       errors++;
-//       if(i === 0 && errors === 1) {
-//         console.log("2nd chance");
-//         current = sequence[i+1];
-//         //continue;
-//       } else {
-//         return false;
-//       }
-//     } else {
-//       errors++;
-//       console.log("F");
-//     }
-//   }
-
-//   return errors <= 1;
-// }
-
-// function strictlyIncreasingSequence(sequence: number[], skipIndex: number): boolean {
-//   let current = 0;
-//   let next = 1;
-//   if (skipIndex === 0) {
-//     current = 1;
-//     next = 2;
-//   } else if (skipIndex === 1) {
-//     next = 2;
-//   }
-
-//   while (current < sequence.length - 1) {
-//     if (sequence[current] >= sequence[next]) {
-//       return false;
-//     }
-//     current++;
-//     next++;
-//     if (skipIndex === current) {
-//       current++;
-//       next++;
-//     } else if (skipIndex === 1) {
-//       next = 2;
-//     }
-//   }
-//   return true;
-// }
-
-// function almostIncreasingSequence(sequence: number[]): boolean {
-//   for (let i = 0; i < sequence.length; i++) {
-//     if (strictlyIncreasingSequence(sequence, i)) {
-//       return true;
-//     }
-//   }
-//   return false;
-// }
-
-// function almostIncreasingSequence(sequence: number[]): boolean {
-//   let foundProblem = false;
-//   for (let i = 0; i < sequence.length - 1; i++) {
-//     if (sequence[i] >= sequence[i + 1]) {
-//       if (sequence[i] < sequence[i + 2]) {
-//         if (foundProblem) {
-//           return false;
-//         }
-//         foundProblem = true;
-//       } else {
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// }
-
-function almostIncreasingSequence(sequence: number[]): boolean {
-  let strikes = 0;
-  let max = -100000000;
-  let secondMax = -100000000;
-
-  for (let i = 0; i < sequence.length; i++) {
-    if (sequence[i] > max) {
-      secondMax = max;
-      max = sequence[i];
-    }
-    else if (sequence[i] > secondMax) {
-      max = sequence[i];
-      strikes++;
-    }
-    else {
-      strikes++;
-    }
-  }
-
-  return strikes < 2;
-}
-
 function cellValue(row: number, column: number, matrix: number[][]): number {
   for (let i = row; i >= 0; i--) {
-    if(matrix[row][i] === 0) {
+    if(matrix[i][column] === 0) {
       return 0;
     }
   }
@@ -134,15 +9,13 @@ function cellValue(row: number, column: number, matrix: number[][]): number {
 
 function matrixElementsSum(matrix: number[][]): number {
   let result = 0;
-  for (let column = 0; column < matrix.length; column++) {
-    for (let row = 0; row < matrix[column].length; row++) {
+  for (let row = 0; row < matrix.length; row++) {
+    for (let column = 0; column < matrix[row].length; column++) {
       result += cellValue(row, column, matrix);
     }
   }
   return result;
 }
-
-
 
 //console.log(almostIncreasingSequence([1, 3, 2]));
 //console.log(almostIncreasingSequence([1, 2, 1, 2]));
---
+++ typescript/wordNumber.ts
@@ -0,0 +1,82 @@
+//Consider a "word" as any sequence of capital letters A-Z (not limited to just "dictionary words"). 
+//For any word with at least two different letters, there are other words composed of the same letters 
+//but in a different order (for instance, STATIONARILY/ANTIROYALIST, which happen to both be dictionary words; 
+//for our purposes "AAIILNORSTTY" is also a "word" composed of the same letters as these two).
+
+//We can then assign a number to every word, based on where it falls in an alphabetically sorted list of 
+//all words made up of the same set of letters. One way to do this would be to generate the entire list of words and find the desired one, but this would be slow if the word is long.
+
+//Write a program which takes a word as a command line argument and prints to standard output its number. 
+//Do not use the method above of generating the entire list. 
+//Your program should be able to accept any word 25 letters or less in length (possibly with some letters repeated), 
+//and should use no more than 1 GB of memory and take no more than 500 milliseconds to run. Any answer we check will fit in a 64-bit integer.
+
+//Sample words, with their rank:
+
+//ABAB = 2
+//AAAB = 1
+//BAAA = 4
+//QUESTION = 24572
+//BOOKKEEPER = 10743
+
+function toLetterMap(input: string) {
+  let letterMap: { [id: string]: number } = {};
+  for (let i = 0; i < input.length; i++) {
+    const letter = input[i];
+    if (!letterMap[letter]) {
+      letterMap[letter] = 0;
+    }
+    letterMap[letter]++;
+  }
+  return letterMap;
+}
+
+function getLetterPosition(letter: string, letters: { [id: string]: number }): number {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  let result = 0;
+  for (let i = 0; i < alphabet.length; i++) {
+    let current = alphabet[i];
+    if (letters[current]) {
+      result++;
+    }
+    if (current === letter) {
+      break;
+    }
+  }
+  return result - 1;
+}
+
+function removeLetter(letter: string, letters: { [id: string]: number }) {
+  letters[letter]--;
+  if (letters[letter] === 0) {
+    delete letters[letter];
+  }
+}
+
+function stringToNumber(input: string): number {
+  const letters = toLetterMap(input);
+  console.log(letters);
+  let result = 0;
+  for (let i = input.length - 1; i >= 0; i--) {
+    const uniqueLetters = Object.keys(letters).length;
+    const place = input.length - i;
+    const letter = input[i];
+    const letterPosition = getLetterPosition(letter, letters);
+    const value = letterPosition * Math.pow(place, uniqueLetters);
+    console.log(letter + ", " + letterPosition + ":" + uniqueLetters + ", " + value + ", " + place);
+    result += value;
+
+    //removeLetter(letter, letters);
+  }
+  return result;
+}
+
+function validate(input: string, expected: number) {
+  const actual = stringToNumber(input);
+  const correct = expected === actual;
+  console.log(`${input}=${correct} actual=${actual} expected=${expected}`);
+}
+
+validate("ABAB", 2);
+validate("AAAB", 1);
+validate("BAAA", 4);
