fabf4b8d57e5016fe624d3721e3eb641c6348608
Jake Pearson <notjakepearson@gmail.com>
2017-08-20 19:27:39 -0600

    Common Character Count

--- typescript/codeFights.ts
+++ typescript/codeFights.ts
@@ -1,15 +1,29 @@
-function allLongestStrings(inputArray: string[]): string[] {
-  let result: string[];
-  let maxLength = -1;
-  for(let i=0; i<inputArray.length; i++) {
-    let current = inputArray[i];
-    if(current.length > maxLength) {
-      result = [];
-      maxLength = current.length;
+function toLetters(input: string) {
+  let letterMap: { [id: string]: number } = {};
+  for (let i = 0; i < input.length; i++) {
+    const letter = input[i];
+    if (!letterMap[letter]) {
+      letterMap[letter] = 0;
     }
-    if(current.length === maxLength) {
-      result.push(current);
+    letterMap[letter]++;
+  }
+  return letterMap;
+}
+
+function commonCharacterCount(s1: string, s2: string): number {
+  let letters = toLetters(s1);
+  let result = 0;
+  for(let i=0; i<s2.length; i++) {
+    let letter = s2[i];
+    if(letters[letter]) {
+      letters[letter]--;
+      result++;
+      if(letters[letter] === 0) {
+        delete letters[letter];
+      }
     }
   }
   return result;
 }
+
+console.log(commonCharacterCount("zzz", "zzzzz"));
