a774ecf552da3e2437bf4772f5d70b52e44b5983
Jake Pearson <jake.pearson@ca.com>
2017-12-17 20:14:01 -0700

    Advent 4

--- typescript/advent.ts
+++ typescript/advent.ts
@@ -1,12 +1,14 @@
 import * as advent1 from './advent/advent-1';
 import * as advent2 from './advent/advent-2';
 import * as advent3 from './advent/advent-3';
+import * as advent4 from './advent/advent-4';
 
 function main() {
   switch (process.argv[2]) {
     case "1": return advent1.main();
     case "2": return advent2.main();
     case "3": return advent3.main();
+    case "4": return advent4.main();
     default: throw new Error("You're not on the list")
   }
 }
