81d93e5d6d657061d9bbcffe0eee8fe754b27c28
Jake Pearson <jake.pearson@ca.com>
2017-12-18 20:40:56 -0700

    Completed 16

--- typescript/advent/advent-16.ts
+++ typescript/advent/advent-16.ts
@@ -18,7 +18,7 @@ function partner(program: string[], programA: string, programB: string) {
 
 function process(program: string[], instruction: string) {
   const operands = instruction.substring(1).split('/')
-  switch(instruction[0]) {
+  switch (instruction[0]) {
     case 's': return spin(program, parseInt(operands[0]))
     case 'x': return exchange(program, parseInt(operands[0]), parseInt(operands[1]))
     case 'p': return partner(program, operands[0], operands[1])
@@ -26,15 +26,38 @@ function process(program: string[], instruction: string) {
   }
 }
 
-function execute(instructions: string): string {
-  const programs = "abcdefghijklmnop".split('')
+function execute(state: string, instructions: string): string {
+  const programs = state.split('')
   instructions.split(',').forEach(instruction => process(programs, instruction))
   return programs.join('')
 }
 
-const littleInput = "s1,x3/4,pe/b"
-const bigInput = fs.readFileSync('advent/advent-16.txt', 'utf8')
+type Test {
+  input: string;
+  defaultState: string;
+}
+
+
+const littleInput: Test = {
+  input: "s1,x3/4,pe/b",
+  defaultState: "abdce"
+}
+const bigInput: Test = {
+  input: fs.readFileSync('advent/advent-16.txt', 'utf8'),
+  defaultState: "abcdefghijklmnop"
+}
 
 export function main() {
-  console.log(execute(bigInput))
+  var state = bigInput.defaultState
+  const oldValues = new Map<string, number>()
+  for (var i = 0; i < 28; i++) {
+    state = execute(state, bigInput.input)
+    if (oldValues.has(state)) {
+      console.log("Found a loop: " + state + "," + i + "," + oldValues.get(state))
+      break;
+    }
+    oldValues.set(state, i)
+  }
+  console.log(state)
+  console.log(Math.pow(1000, 3) % 36)
 }
