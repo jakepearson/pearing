2859c7136ef3692c03f11d6ce31995f056259538
Jake Pearson <notjakepearson@gmail.com>
2017-12-21 19:37:43 -0700

    Completed advent 3

--- typescript/advent.ts
+++ typescript/advent.ts
@@ -3,6 +3,7 @@ import * as advent2 from './advent/advent-2';
 import * as advent3 from './advent/advent-3';
 import * as advent4 from './advent/advent-4';
 import * as advent5 from './advent/advent-5';
+import * as advent6 from './advent/advent-6';
 import * as advent8 from './advent/advent-8';
 import * as advent16 from './advent/advent-16';
 import * as advent17 from './advent/advent-17';
@@ -14,6 +15,7 @@ function main() {
     case "3": return advent3.main();
     case "4": return advent4.main();
     case "5": return advent5.main();
+    case "6": return advent6.main();
     case "8": return advent8.main();
     case "16": return advent16.main();
     case "17": return advent17.main();
--- typescript/advent/advent-3.ts
+++ typescript/advent/advent-3.ts
@@ -22,24 +22,98 @@ function getMiddles(radius: number): number[] {
   return result;
 }
 
+function getCorners(radius: number): number[] {
+  const result: number[] = [];
+  const edge = radius * 2;
+  const start = Math.pow(edge + 1, 2);
+  for (var i = 0; i < 4; i++) {
+    result.push(start - i * edge);
+  }
+  return result;
+}
+
 function distance(input: number): number {
-  const distanceToRing = Math.floor(Math.ceil(Math.sqrt(input)) / 2);
+  const distanceToRing = getRadius(input)
   const middles = getMiddles(distanceToRing);
   var distanceToMiddle = Number.MAX_SAFE_INTEGER;
   middles.forEach(middle => {
     const difference = Math.abs(middle - input);
-    if(difference < distanceToMiddle) {
+    if (difference < distanceToMiddle) {
       distanceToMiddle = difference;
     }
   })
   return distanceToRing + distanceToMiddle;
 }
 
-export function main() {
+function getRadius(input: number) {
+  return Math.floor(Math.ceil(Math.sqrt(input)) / 2);
+}
+
+function coordinatesAt(input: number): number[] {
+  const radius = getRadius(input)
+  const corners = getCorners(radius)
+  if (input > corners[1]) {
+    return [radius - corners[0] + input, -radius]
+  }
+  if (input > corners[2]) {
+    return [-radius, -radius + corners[1] - input]
+  }
+  if (input > corners[3]) {
+    return [-radius + corners[2] - input, radius]
+  }
+  return [radius, radius - corners[3] + input]
+}
+
+function part1() {
   cases.forEach(c => {
     const result = distance(c.input);
     const success = result === c.expected ? "Good" : "Bad ";
-    console.log(success + ": " + c.input + ", wanted: " + c.expected + " but was: " + result)
-  })
+    console.log(success + ": " + c.input + ", wanted: " + c.expected + " but was: " + result);
+  });
+}
 
+function test(input: number) {
+  console.log(input + " " + coordinatesAt(input))
+}
+
+function getAt(state: Map<string, number>, x: number, y: number): number {
+  const key = x + "-" + y
+  const result = state.get(key) || 0
+  return result
+}
+
+function sumAt(state: Map<string, number>, x: number, y: number) {
+  const sum = 
+    getAt(state, x + 1, y) +
+    getAt(state, x + 1, y + 1) +
+    getAt(state, x,     y + 1) +
+    getAt(state, x - 1, y + 1) +
+    getAt(state, x - 1, y) +
+    getAt(state, x - 1, y - 1) +
+    getAt(state, x,     y - 1) +
+    getAt(state, x + 1, y - 1);
+  return sum;
+}
+
+function part2(input: number) {  
+  const state = new Map<string, number>()
+  state.set("0-0", 1)
+  var sum = 1
+  var counter = 2
+  while (sum <= input) {
+    const coordinates = coordinatesAt(counter)
+    const x = coordinates[0]
+    const y = coordinates[1]
+    sum = sumAt(state, x, y);
+    state.set(x + "-" + y, sum)
+    counter++
+  }
+  console.log(sum)
+}
+
+
+
+export function main() {
+  //part1();
+  part2(347991)
 }
