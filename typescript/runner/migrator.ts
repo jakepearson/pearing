d51614cd476472fe39769fab3e9f7197f7056814
Jake Pearson <jake.pearson@ca.com>
2018-02-27 16:30:02 -0700

    Rotate digits

---
+++ typescript/runner/migrator.ts
@@ -0,0 +1,29 @@
+import * as axios from 'axios'
+
+interface AvroSchema {
+  Name: string
+  Topic: string
+  Fields: { [key: string]: string }
+}
+
+const testSchema: AvroSchema = {
+  Name: "Test",
+  Topic: "test-topic",
+  Fields: {
+    id: "string"
+  }
+}
+
+async function upsertSchema(schema: AvroSchema): Promise<boolean> {
+  const payload = Object.keys(schema.Fields).map((key) => { return { name: key, type: schema.Fields[key] } })
+  const requestConfig: axios.AxiosRequestConfig = {
+    headers: { 'Content-Type': 'application/vnd.schemaregistry.v1+json' },
+    data: payload
+  }
+  const result = await axios.default.post("http://kafka-01:8081/subjects/ac-user-event-value/versions", requestConfig)
+  return true
+}
+
+export function main() {
+  upsertSchema(testSchema)
+}
---
+++ typescript/runner/rotate.ts
@@ -0,0 +1,39 @@
+function rotateDigit(input: number): number {
+  switch (input) {
+    case 0:
+    case 1:
+    case 8: return 0
+    case 2:
+    case 5:
+    case 6:
+    case 9: return 1
+    default: return -1
+  }
+}
+
+function canRotate(input: number): boolean {
+  let total = 0
+  while (input > 0) {
+    const next = rotateDigit(input % 10)
+    if (next < 0) {
+      return false
+    }
+    total += next
+    input = Math.floor(input / 10)
+  }
+  return total > 0
+}
+
+function rotatedDigits(input: number): number {
+  let result = 0
+  for (let i = 1; i <= input; i++) {
+    if (canRotate(i)) {
+      result++
+    }
+  }
+  return result
+}
+
+export function main() {
+  console.log(rotatedDigits(10))
+}
