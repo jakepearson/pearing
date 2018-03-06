995e4d64a83cee8e3144d49d01c6e7086edc2bbf
Jake Pearson <jake.pearson@ca.com>
2018-03-06 16:36:56 -0700

    Stocks 4

--- typescript/runner/stocks.ts
+++ typescript/runner/stocks.ts
@@ -1,21 +1,35 @@
-import * as Collections from 'typescript-collections';
+//import * as Collections from 'typescript-collections';
 
-function maxProfit(quotes: number[]): number {
+function maxProfit(maxTrades: number, quotes: number[]): number {
+  const trades: number[] = []
+
   let maxProfit = 0
-  let minPrice = quotes[0]
+  let peakIndex = 0
+  let valleyIndex = 0
   for (let i = 0; i < quotes.length; i++) {
-    if (quotes[i] < minPrice) {
-      minPrice = quotes[i]
+    while (i < quotes.length - 1 && quotes[i + 1] < quotes[i]) {
+      i++
     }
-    const profit = quotes[i] - minPrice
-    if (profit > maxProfit) {
-      maxProfit = profit
+    valleyIndex = i
+    peakIndex = i
+
+    while (i < quotes.length - 1 && quotes[i + 1] > quotes[i]) {
+      i++
     }
+    peakIndex = i
+    const profit = quotes[peakIndex] - quotes[valleyIndex]
+    trades.push(profit)
+    peakIndex = i
+    valleyIndex = i
+  }
+  trades.sort((t1, t2) => t2 - t1)
+  for (let i = 0; i < 2; i++) {
+    maxProfit += trades[i]
   }
   return maxProfit
 }
 
 export function main() {
-  const input = [7, 1, 5, 3, 6, 4]
-  console.log(maxProfit(input))
+  const input = [5, 2, 3, 4, 2, 5, 1, 5]
+  console.log(maxProfit(2, input))
 }
