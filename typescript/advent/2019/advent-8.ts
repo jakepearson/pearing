86acacf13954f62a5cf8a3b940cbe70430562616
Jake Pearson <jopears@microsoft.com>
2019-12-11 13:07:08 -0700

    8-2

--- typescript/advent/2019/advent-8.ts
+++ typescript/advent/2019/advent-8.ts
@@ -7,25 +7,25 @@ function count(input: string, match: string): number {
   return input.split(match).length - 1
 }
 
-function findColorAtPosition(data: string, x: number, y: number): number {
+function findColorAtPosition(data: string, x: number, y: number, width: number, height: number): string {
+  let color = "2"
+  let offset = y * width + x
   do {
-
+    color = data[offset]
+    offset += width * height
   }
+  while (color === "2")
+  return color
 }
 
 export function main() {
-  const pageSize = 25 * 6
-  let offset = 0
-  let lowest = Number.MAX_SAFE_INTEGER
-  let result = 0
-  while (offset < bigInput.length) {
-    const page = bigInput.substring(offset, offset + pageSize)
-    const zeroes = count(page, "0")
-    if (zeroes < lowest) {
-      result = count(page, "1") * count(page, "2")
-      lowest = zeroes
+  const width = 25
+  const height = 6
+  for (let y = 0; y < height; y++) {
+    let line = ""
+    for (let x = 0; x < width; x++) {
+      line += findColorAtPosition(bigInput, x, y, width, height)
     }
-    offset += pageSize
+    console.log(line)
   }
-  console.log(result)
 }
