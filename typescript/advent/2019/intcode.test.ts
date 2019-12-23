20556929f339e8af717bff9ae0abcc7efc1e394a
Jake Pearson <jopears@microsoft.com>
2019-12-23 16:36:19 -0700

    Multiply and add working

--- typescript/advent/2019/intcode.test.ts
+++ typescript/advent/2019/intcode.test.ts
@@ -12,17 +12,9 @@ test('add (position mode)', () => {
 })
 
 test('add (position mode 2)', () => {
-  //validate("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")
+  validate("1,1,1,4,99,5,6,0,4,0,4,4,99", [], [30, 2])
 })
 
 test("multiply (position mode)", () => {
-  //validate("2,4,4,5,99,0", "2,4,4,5,99,9801")
-})
-
-test("input", () => {
-
-})
-
-test("output", () => {
-
+  validate("2,6,6,7,4,7,99,0", [], [9801])
 })
