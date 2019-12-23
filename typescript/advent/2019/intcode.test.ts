0315fde02ea2638b1a5e829a19f3593756c2f299
Jake Pearson <jopears@microsoft.com>
2019-12-23 16:25:31 -0700

    Test to run a simple program

--- typescript/advent/2019/intcode.test.ts
+++ typescript/advent/2019/intcode.test.ts
@@ -1,11 +1,28 @@
 import * as intcode from './intcode'
 
-test('add', () => {
-  const program = intcode.parse("1,0,0,0,99")
-  intcode.run(program)
-  expect(program[0]).toEqual(2)
+function validate(program: string, input: number[], expectedOutput: number[]): number[] {
+  const data = intcode.parse(program)
+  const output = intcode.run(data, input)
+  expect(output).toStrictEqual(expectedOutput)
+  return data
+}
+
+test('add (position mode)', () => {
+  const data = validate("1,0,0,0,4,0,99", [], [2])
 })
 
-test("x", () => {
-  expect(0).toEqual(0)
+test('add (position mode 2)', () => {
+  //validate("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")
+})
+
+test("multiply (position mode)", () => {
+  //validate("2,4,4,5,99,0", "2,4,4,5,99,9801")
+})
+
+test("input", () => {
+
+})
+
+test("output", () => {
+
 })
--- typescript/advent/2019/intcode.ts
+++ typescript/advent/2019/intcode.ts
@@ -1,35 +1,28 @@
-import * as util from '../util'
-import * as readline from 'readline-sync'
-
-function get(offset: number, mode: string, data: number[]): number {
-  //console.log(offset, mode)
+function get(operationPointer: number, parameterOffset: number, mode: string, data: number[]): number {
   switch (mode) {
     case "0":
-      return data[data[offset]]
+      return data[data[operationPointer + parameterOffset]]
     case "1":
-      return data[offset]
-    case "2":
-      return data[data[offset + globalOffset]]
+      return data[operationPointer + parameterOffset]
     default:
       throw "Unknown addressing mode"
   }
 }
 
 function set(value: number, offset: number, data: number[]) {
-  console.log("set: " + value)
   data[offset] = value
 }
 
 function math(operation: string, offset: number, data: number[], f: (x: number, y: number) => number) {
-  const operand1 = get(offset + 1, operation[2], data)
-  const operand2 = get(offset + 2, operation[1], data)
+  const operand1 = get(offset, 1, operation[2], data)
+  const operand2 = get(offset, 2, operation[1], data)
 
   if (operation[0] === "1") {
     throw `Write parameter cannot be immediate mode: ${operation}`
   }
 
   let output = f(operand1, operand2)
-  let outputIndex = get(offset + 3, "1", data)
+  let outputIndex = get(offset, 3, "1", data)
 
   set(output, outputIndex, data)
 }
@@ -42,37 +35,35 @@ function multiply(operation: string, offset: number, data: number[]) {
   return math(operation, offset, data, (x, y) => x * y)
 }
 
-function input(operation: string, offset: number, data: number[]) {
-  const operand1 = get(offset + 1, "1", data)
-  const input = globalInputs.shift().toString()//readline.question("Input please\n")
+function input(operation: string, offset: number, data: number[], inputData: number[]) {
+  const operand1 = get(offset, 1, "1", data)
+  const input = inputData.shift().toString()
   set(Number.parseInt(input), operand1, data)
 }
 
-function output(operation: string, offset: number, data: number[]) {
-  const operand1 = get(offset + 1, operation[2], data)
-  globalOutput = operand1
-  console.log(operand1)
+function output(operation: string, offset: number, data: number[], outputData: number[]) {
+  const operand1 = get(offset, 1, operation[2], data)
+  outputData.push(operand1)
 }
 
-function setOffset(operation: string, offset: number, data: number[]) {
-  globalOffset = get(offset + 1, operation[2], data)
-  console.log("setOffset: " + globalOffset)
+function setOffset(operation: string, offset: number, data: number[]): number {
+  return get(offset, 1, operation[2], data)
 }
 
 function jumpIfTrue(operation: string, offset: number, data: number[]): number | null {
-  const operand1 = get(offset + 1, operation[2], data)
+  const operand1 = get(offset, 1, operation[2], data)
   if (operand1 === 0) {
     return null
   }
-  return get(offset + 2, operation[1], data)
+  return get(offset, 2, operation[1], data)
 }
 
 function jumpIfFalse(operation: string, offset: number, data: number[]) {
-  const operand1 = get(offset + 1, operation[2], data)
+  const operand1 = get(offset, 1, operation[2], data)
   if (operand1 !== 0) {
     return null
   }
-  return get(offset + 2, operation[1], data)
+  return get(offset, 2, operation[1], data)
 }
 
 function lessThan(operation: string, offset: number, data: number[]) {
@@ -83,8 +74,8 @@ function equal(operation: string, offset: number, data: number[]) {
   math(operation, offset, data, (x, y) => x === y ? 1 : 0)
 }
 
-function processOperation(offset: number, data: number[]): (number | null) {
-  const operation = get(offset, "1", data).toString().padStart(5, "0")
+function processOperation(offset: number, data: number[], inputData: number[], outputData: number[]): (number | null) {
+  const operation = get(offset, 0, "1", data).toString().padStart(5, "0")
   const code = operation[3] + operation[4]
   switch (code) {
     case "01":
@@ -94,10 +85,10 @@ function processOperation(offset: number, data: number[]): (number | null) {
       multiply(operation, offset, data)
       return offset + 4
     case "03":
-      input(operation, offset, data)
+      input(operation, offset, data, inputData)
       return offset + 2
     case "04":
-      output(operation, offset, data)
+      output(operation, offset, data, outputData)
       return offset + 2
     case "05":
       const trueOffset = jumpIfTrue(operation, offset, data)
@@ -126,10 +117,11 @@ function processOperation(offset: number, data: number[]): (number | null) {
   }
 }
 
-export function run(data: number[]): number[] {
+export function run(data: number[], input: number[]): number[] {
   let offset = 0
+  let output: number[] = []
   do {
-    const newOffset = processOperation(offset, data)
+    const newOffset = processOperation(offset, data, input, output)
     if (newOffset == null) {
       break;
     }
@@ -137,22 +129,10 @@ export function run(data: number[]): number[] {
 
   }
   while (offset < data.length)
-  return data
+
+  return output
 }
 
 export function parse(input: string): number[] {
   return input.split(',').map(s => Number.parseInt(s))
-}
-
-const smallInput = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"
-const small2 = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
-const small3 = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
-const bigInput = util.read(`${__dirname}/advent-7.txt`)
-
-let globalInputs: number[] = []
-let globalOutput: number
-let globalOffset: number
-
-export function main() {
-  run(parse(smallInput))
 }
