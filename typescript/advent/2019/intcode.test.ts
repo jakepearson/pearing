f41401fc2334923ec76b2edd87eb1efd35dd8047
Jake Pearson <jopears@microsoft.com>
2019-12-23 16:54:13 -0700

    Less than/equals/immediate mode

--- typescript/advent/2019/intcode.test.ts
+++ typescript/advent/2019/intcode.test.ts
@@ -18,3 +18,23 @@ test('add (position mode 2)', () => {
 test("multiply (position mode)", () => {
   validate("2,6,6,7,4,7,99,0", [], [9801])
 })
+
+test("equal (position mode)", () => {
+  validate("3,9,8,9,10,9,4,9,99,-1,8", [8], [1])
+  validate("3,9,8,9,10,9,4,9,99,-1,8", [2], [0])
+})
+
+test("less than (position mode)", () => {
+  validate("3,9,7,9,10,9,4,9,99,-1,8", [7], [1])
+  validate("3,9,7,9,10,9,4,9,99,-1,8", [8], [0])
+})
+
+test("equals (immediate mode)", () => {
+  validate("3,3,1108,-1,8,3,4,3,99", [8], [1])
+  validate("3,3,1108,-1,8,3,4,3,99", [2], [0])
+})
+
+test("less than (immediate mode)", () => {
+  validate("3,3,1107,-1,8,3,4,3,99", [7], [1])
+  validate("3,3,1107,-1,8,3,4,3,99", [8], [0])
+})
