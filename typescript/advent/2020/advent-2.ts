d2ca8afd8b11cccb3d2089777377e2e4291f046d
jakepearson <jake@stedi.com>
2020-12-02 16:09:31 -0700

    Advent 2020 2.2

--- typescript/advent/2020/advent-2.ts
+++ typescript/advent/2020/advent-2.ts
@@ -1,23 +1,21 @@
+import { match } from 'assert';
 import * as util from '../util';
 
 const sample = '1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc';
 const real = util.read(`${__dirname}/advent-2.txt`);
 
-function countLetter(letter: string, password: string): number {
-  return password.split(letter).length - 1;
-}
-
 function validPassword(entry: string): boolean {
   try {
     const entryParts = entry.split(' ');
     const countParts = entryParts[0].split('-');
-    const minCount = parseInt(countParts[0], 10);
-    const maxCount = parseInt(countParts[1], 10);
+    const index1 = parseInt(countParts[0], 10) - 1;
+    const index2 = parseInt(countParts[1], 10) - 1;
     const letter = entryParts[1][0];
     const password = entryParts[2];
-    const count = countLetter(letter, password);
 
-    return count >= minCount && count <= maxCount;
+    const match1 = password[index1] === letter ? 1 : 0;
+    const match2 = password[index2] === letter ? 1 : 0;
+    return match1 + match2 === 1;
   } catch (e) {
     console.log(`Entry: ${entry}`);
     throw e;
