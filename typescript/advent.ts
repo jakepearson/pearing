b455db743981fbfb4fe745e1cf2e55a7fcaf555c
Jake Pearson <jake.pearson@broadcom.com>
2024-12-16 13:19:54 -0700

    feat: switch to bun runner

--- typescript/advent.ts
+++ typescript/advent.ts
@@ -1,5 +1,7 @@
 const year = process.argv[2];
 const adventId = process.argv[3];
 
-// eslint-disable-next-line no-eval
-eval(`const advent = require("./advent/${year}/advent-${adventId}");advent.main()`);
+const advent = await import(`./advent/${year}/advent-${adventId}`);
+advent.main();
+
+export {};
